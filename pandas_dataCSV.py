181d555263219b04dc672cd82f258a108f907983
Tanaya25997 <tanaya.k.25@gmail.com>
2024-06-17 14:18:58 -0400

    datacsv file done

--- pandas_dataCSV.py
+++ pandas_dataCSV.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def preprocess_datacsv():
@@ -13,12 +14,62 @@ def preprocess_datacsv():
     print(df.info())
     
     
+    '''
     ### dropping empty cells ###
     
     df_dropped = df.dropna()   
     print("\n ============ Printing Data after dropping empty cells  =============== \n", df_dropped.to_string())
     print("\n ================================== Quick net overview of data ================================== \n")
     print(df_dropped.info())
+    '''
+    
+    ### empty cells in Date (object) and Calories (float). 
+    ### filling empty cells in  Calories with mean
+    ### dropping empty cells in Date 
+
+    df = df.dropna(subset=['Date'])
+    df['Calories'] = df['Calories'].fillna(df['Calories'].mean())
+    print("\n ======================= Printing Data ================================== \n", df.to_string())
+    print("\n ================================== Quick net overview of data ================================== \n")
+    print(df.info())
+    
+    #### changing wrong format 
+    df['Date'] = pd.to_datetime(df['Date'])
+    print("\n ======================= Printing Data ================================== \n", df.to_string())
+    print("\n ================================== Quick net overview of data ================================== \n")
+    print(df.info())
+    
+    ### changing wrong data in 'Duration' with the Mode of the value 
+    mode_duration = df['Duration'].mode()[0]
+    for d in df.index:
+        if df.loc[d,'Duration'] > 60:
+            df.loc[d,'Duration'] = mode_duration
+            
+    print("\n ======================= Printing Data ================================== \n", df.to_string())
+    print("\n ================================== Quick net overview of data ================================== \n")
+    print(df.info())
+    
+    
+    ### Drop Duplicates 
+    df.drop_duplicates(inplace=True)
+    print("\n ======================= Printing Data ================================== \n", df.to_string())
+    print("\n ================================== Quick net overview of data ================================== \n")
+    print(df.info())
+    
+    ## Plot 
+    print(df.corr())
+    #df.plot(kind = 'scatter', x = 'Duration', y = 'Calories')
+    #plt.show()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
